57fbb5b728aacac1d28b750fbf29ebc9d14c6231
Kirill <dads@sad>
2020-04-13 23:54:29 +0400

    modal changes

--- src/componnets/UI/SelectModal/index.js
+++ src/componnets/UI/SelectModal/index.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
-
 import {
-  withModalRootContext,
   Spinner,
   Group,
   Cell,
-  View,
-  PanelHeader,
+  Card,
   List,
-  Panel,
   Avatar,
   ModalRoot,
   ModalCard,
+  ModalPageHeader,
 } from "@vkontakte/vkui";
 import { connect } from "react-redux";
+
+import "./SelectModal.scss";
 
 class SelectModal extends Component {
   state = {
@@ -21,27 +20,42 @@ class SelectModal extends Component {
     isLoading: false,
   };
 
-
   componentDidMount() {}
+
+  getDataGroup = (e) => {
+    console.log('HEY FROM CLICK ====>', e)
+
+    // вызываем action.setGroup(e) передаем это значение в reducer
+    // вызываем action.setAnotherPage() делаем переход на другую страницу
+    // на новой странице получаем id группы и вытаскиваем картинку и название группы
+    // вызываем action.getCurrentGroupToken() для запроса токена что бы менять истории
+  }
 
   render() {
     let groupList = null;
     if (this.props.groupData) {
       groupList = this.props.groupData.map((group, index) => {
         return (
-          <Cell key={index} before={<Avatar size={40} src={group.photo_200} />} selectable>
+          <Cell
+            key={group.id}
+            id={group.id}
+            before={<Avatar size={40} src={group.photo_200} />}
+            selectable
+            onClick={() => this.getDataGroup(group.id)}
+          >
             <span>{group.name}</span>
           </Cell>
         );
       });
     }
     return (
-      <ModalRoot  activeModal={this.props.modalId}>
+      <ModalRoot activeModal={this.props.modalId}>
         <ModalCard
+          header="Выберите группу"
           onClose={this.props.closeModal}
           id="select"
         >
-          {groupList}
+          <List>{groupList}</List>
         </ModalCard>
       </ModalRoot>
     );
